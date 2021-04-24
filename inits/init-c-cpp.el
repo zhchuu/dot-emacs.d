660b022edd9b4c4672691e40de4201f2f8108762
mia.chuck <zhongchh7@mail2.sysu.edu.cn>
2021-04-24 18:49:43 +0800

    eglot is all you need for C/C++ env.

--- inits/init-c-cpp.el
+++ inits/init-c-cpp.el
@@ -2,98 +2,52 @@
 ;;; Commentary:
 
 ;; The C/C++ development environment includes auto completion, syntax check,
-;; disassemble, navigation, and code browsing.
+;; disassemble, navigation, refactoring, and code browsing.
 
-;; About Auto completion:
-;; The campany is the frontend and the irony is the backend.
-;; Install clang and LLVM before M-x irony-install-server.
+;; About Auto completion: eglot + company
 ;; Use M-n and M-p to select candidate items.
 
-;; About syntax check:
-;; Again I use irony as the backend and the frontend becomes the flycheck.
+;; About syntax check: eglot + flycheck
 ;; Use C-c ! l command to display errors and warnings.
+
+;; About navigation and refactoring: eglot + clangd
+;; Follow the official instructions to install clangd.
+;; clangd --version to ensure the installation.
 
 ;; About disassemble:
 ;; M-x disaster
-
-;; About navigation and refactoring:
-;; Compile and install rtags by hand.
-;; M-x cmake-ide-compile to compile the configured cmake project within Emacs.
 
 ;; About coding browsing:
 ;; Use ECB package (Emacs Code Browsing).
 ;; M-x ecb-activate command to enable ECB.
 ;; The rebuild-method-buffer in ECB works only if the semantic is enabled.
+;; **Note**: The ECB has it own windows management system, which doesn't get
+;; along with any other windows (flycheck error window).  I may fix it later.
 
 ;;; Code:
 
-;; Package installation of cmake-ide
-(require-package 'cmake-ide)
-
+;; Set the tab width
 (setq c-basic-offset 4)
 
+;; Code navivation ---
+(require-package 'eglot)
+(require 'eglot)
+(add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+
 ;; Auto completion ---
-(require-package 'irony)
 (require-package 'company)
-(require-package 'company-irony)
-(require-package 'company-c-headers)
-(require-package 'irony-eldoc)
-(require-package 'company-irony-c-headers)
-
-
 (require 'company)
-(require 'irony)
-(require 'company-irony)
-(require 'company-c-headers)
-(require 'irony-eldoc)
-(require 'company-irony-c-headers)
-(add-hook 'c++-mode-hook 'company-mode)
 (add-hook 'c-mode-hook 'company-mode)
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-(add-hook 'irony-mode-hook 'irony-eldoc)
-(add-hook 'eldoc-mode-hook #'(lambda ()
-				 (setf eldoc-idle-delay 0.5)))
-
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 2)
-(setq company-dabbrev-downcase nil)
-
-(setf company-backends '())  ;; removes all the predefined backends at first
-(add-to-list 'company-backends 'company-dabbrev)
-(add-to-list 'company-backends 'company-dabbrev-code)
-(add-to-list 'company-backends 'company-files)
-(add-to-list 'company-backends 'company-semantic)
-(add-to-list 'company-backends 'company-keywords)
-(add-to-list 'company-backends 'company-irony)
-(add-to-list 'company-backends 'company-capf)
-(add-to-list 'company-backends 'company-c-headers)
-(add-to-list 'company-backends 'company-irony-c-headers)
-
-;; (add-to-list 'company-backends 'company-gtags)
-
-;; (add-to-list 'company-c-headers-path-system "/usr/include/c++/4.9.2/")
-;; (global-set-key (kbd "C-;") 'company-complete-common)
-
-;; Company color setting
-;; (defun theme-dark ()
-;;   (interactive)
-;;    (set-face-foreground 'company-tooltip "#000")
-;;     (set-face-background 'company-tooltip "#fff")
-;;      (set-face-foreground 'company-scrollbar-bg "#fff")
-;;       (set-face-background 'company-scrollbar-fg "#999")
-;; )
-;; (theme-dark)
+(add-hook 'c++-mode-hook 'company-mode)
 
 ;; Syntax check ---
 (require-package 'flycheck)
-(require-package 'flycheck-irony)
-(autoload 'zhchuu/flycheck "init-flycheck")
 (require 'flycheck)
+(autoload 'zhchuu/flycheck "init-flycheck")
 (add-hook 'c++-mode-hook 'global-flycheck-mode)
 (add-hook 'c-mode-hook 'global-flycheck-mode)
-(add-hook 'flycheck-mode-hook 'flycheck-irony-setup)
 (add-hook 'prog-mode-hook 'zhchuu/flycheck)
 
 ;; Disassemble ---
@@ -130,29 +84,6 @@ MODE: the major programming mode"
 (add-hook 'c++mode-hook #'(lambda ()) (zhchuu/semantic 'c++-mode))
 (add-hook 'cmode-hook #'(lambda ()) (zhchuu/semantic 'c-mode))
 
-;; Toggle flycheck with a key
-;;(defun toggle-flycheck-error-buffer ()
-;;  "toggle a flycheck error buffer."
-;;  (interactive)
-;;  (if (string-match-p "Flycheck errors" (format "%s" (window-list)))
-;;      (dolist (w (window-list))
-;;        (when (string-match-p "*Flycheck errors*" (buffer-name (window-buffer w)))
-;;          (delete-window w)
-;;          ))
-;;    (flycheck-list-errors)
-;;    )
-;;  )
-;;(global-set-key (kbd "<f4>") 'toggle-flycheck-error-buffer)
-
-;; Code navivation ===
-;(require-package 'helm)
-;(require-package 'helm-rtags)
-;(require 'rtags)
-;(cmake-ide-setup)
-
-;(add-hook 'c++-mode-hook 'trivialfis/rtags)
-;(add-hook 'c-mode-hook 'trivialfis/rtags)
-;(setq rtags-display-result-backend 'helm)
 
 (provide 'init-c-cpp)
 ;;; init-c-cpp.el ends here
